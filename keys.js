e9e54233b49286d6d97425d6fbb580424c13e0d0
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-24 15:20:19 -0500

    spotify and omdb functional!

--- keys.js
+++ keys.js
@@ -1,42 +1,5 @@
 require("dotenv/config");
-const axios = require("axios");
-let input = process.argv[3];
 
-// axios({
-//   method: "GET",
-//   url: `https://rest.bandsintown.com/artists/"${artist}"/events?app_id=codingbootcamp`
-// })
-//   .then(response => {
-//     response.data;
-//     console.log(response.data);
-//   })
-//   .catch(response => {
-//     console.log(`This ${response} failed`);
-//   });
-
-// concertFunction() = () => {};
-
-movieFunction = axios({
-  method: "GET",
-  url: `http://www.omdbapi.com/?apikey=trilogy&t=${input}`
-})
-  .then(response => {
-    console.log(`Title: ${response.data.Title}`);
-    console.log(`Year: ${response.data.Year}`);
-    console.log(`IMDB Rating: ${response.data.imdbRating}`);
-    console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
-    console.log(`Country: ${response.data.Country}`);
-    console.log(`Language: ${response.data.Language}`);
-    console.log(`Plot: ${response.data.Plot}`);
-    console.log(`Actors: ${response.data.Actors}`);
-  })
-  .catch(response => {
-    console.log(`Well that ${response} didn't work!`);
-  });
-
-exports.movies = {
-  movie: movieFunction
-};
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
--- liri.js
+++ liri.js
@@ -1,13 +1,62 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+const Spotify = require("node-spotify-api");
+const spotify = new Spotify(keys.spotify);
+let input = process.argv[3];
+const axios = require("axios");
+
+let spotifyFunction = spotify
+  .search({ type: "track", query: `${input}` })
+  .then(response => {
+    console.log(response.tracks.items[0].artists[0].name);
+    console.log(response.tracks.items[0].name);
+    console.log(response.tracks.items[0].external_urls.spotify);
+    console.log(response.tracks.items[0].album.name);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 // console.log(keys.spotify);
+
+//Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")
+// concertFunction = axios({
+//   method: "GET",
+//   url: `https://rest.bandsintown.com/artists/"${input}"/events?app_id=codingbootcamp`
+// })
+//   .then(response => {
+//     response.data;
+//     console.log(response);
+//   })
+//   .catch(response => {
+//     console.log(`This ${response} failed`);
+//   });
+
+movieFunction = axios({
+  method: "GET",
+  url: `http://www.omdbapi.com/?apikey=trilogy&t=${input}`
+})
+  .then(response => {
+    console.log(`Title: ${response.data.Title}`);
+    console.log(`Year: ${response.data.Year}`);
+    console.log(`IMDB Rating: ${response.data.imdbRating}`);
+    console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+    console.log(`Country: ${response.data.Country}`);
+    console.log(`Language: ${response.data.Language}`);
+    console.log(`Plot: ${response.data.Plot}`);
+    console.log(`Actors: ${response.data.Actors}`);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 switch (process.argv[2]) {
   case "concert-this":
-    // concertFunction();
+    // concertFunction;
     break;
   case "spotify-this-song":
-    // spotifyFunction();
+    spotifyFunction;
     break;
   case "movie-this":
     movieFunction;
